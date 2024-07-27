bd8580bb27cea9e3fbfca3b641db787274ef1e59
fullname <username@localhost.localdomain>
2024-07-27 15:48:15 +0300

    fpm 502

--- setup.bash
+++ setup.bash
@@ -1,4 +1,17 @@
+sudo zypper install make docker-compose docker git
+
+#!/bin/bash
+
+# SSH portunu etkinleştir
+sudo firewall-cmd --permanent --zone=public --add-port=22/tcp
+
+# 80 ve 443 numaralı portları etkinleştir
+sudo firewall-cmd --permanent --zone=public --add-port=80/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=443/tcp
+
+# Değişiklikleri uygula
+sudo firewall-cmd --reload
+
+
 systemctl start docker
 systemctl enable docker
-
-sudo zypper install make docker-compose docker
